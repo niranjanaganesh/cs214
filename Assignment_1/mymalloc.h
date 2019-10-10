36b11f2f2b2895cae6c544c4200d1b6c616b819a
Muhmmad Z Choudhary <39873700+mzc13@users.noreply.github.com>
2019-10-10 17:03:42 -0400

    Update mymalloc.h

--- Assignment_1/mymalloc.h
+++ Assignment_1/mymalloc.h
@@ -1,7 +1,12 @@
+#ifndef _MYMALLOC_H
+#define _MYMALLOC_H
+
 #define malloc(x) mymalloc(x, __FILE__, __LINE__)
 #define free(x) myfree(x, __FILE__, __LINE__)
 
 static char myBlock[4096];
 
-void* mymalloc(int size, char* file, char* line);
-void* myfree(void* ptr, char* file, char* line);
+void* mymalloc(int size, char* file, int line);
+void* myfree(void* ptr, char* file, int line);
+
+#endif
